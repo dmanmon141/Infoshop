57e2a507456acd8fedbcc566b2560e7f41f41f45
dmanmon141 <dmanmon141@g.educaand.es>
2025-06-11 08:46:32 +0200

    Rediseño de página de rastrear paquete

--- frontend/rastrear.php
+++ frontend/rastrear.php
@@ -1,6 +1,6 @@
 <?php
 
-$servidor= "localhost";
+$servidor = "localhost";
 $usuario = "root";
 $password = "";
 $conexion = mysqli_connect($servidor, $usuario, $password);
@@ -17,31 +17,31 @@ if (isset($_POST['correo'])) {
   $usuloginsesion = mysqli_fetch_assoc($usuloginsql);
   $_SESSION['usunom'] = $usuloginsesion['USUNOM'];
 
-  
+
   $usucod = "SELECT USUCOD FROM usuarios WHERE USUCOR = '$correo';";
   $usucodsql = mysqli_query($conexion, $usucod);
-  $usucodsesion =  mysqli_fetch_assoc($usucodsql);
+  $usucodsesion = mysqli_fetch_assoc($usucodsql);
   $_SESSION['usucod'] = $usucodsesion['USUCOD'];
 
-  
 
-}elseif(!isset($_SESSION['usucod'])){
+
+} elseif (!isset($_SESSION['usucod'])) {
   session_destroy();
 }
 
-if(isset($_SESSION['usucod'])){
+if (isset($_SESSION['usucod'])) {
 
-$usucodsesion = $_SESSION['usucod'];
+  $usucodsesion = $_SESSION['usucod'];
 
-$datosql = "SELECT * FROM usuarios WHERE USUCOD = '$usucodsesion';";
-$datosquery = mysqli_query($conexion, $datosql);
-$datosarray = mysqli_fetch_assoc($datosquery);
-$usunom = $datosarray['USUNOM'];
-$usuape = $datosarray['USUAPE'];
-$usucor = $datosarray['USUCOR'];
-$usucont = $datosarray['USUCONT'];
-$usuimg = $datosarray['USUIMG'];
-$usuadm = $datosarray['USUADM'];
+  $datosql = "SELECT * FROM usuarios WHERE USUCOD = '$usucodsesion';";
+  $datosquery = mysqli_query($conexion, $datosql);
+  $datosarray = mysqli_fetch_assoc($datosquery);
+  $usunom = $datosarray['USUNOM'];
+  $usuape = $datosarray['USUAPE'];
+  $usucor = $datosarray['USUCOR'];
+  $usucont = $datosarray['USUCONT'];
+  $usuimg = $datosarray['USUIMG'];
+  $usuadm = $datosarray['USUADM'];
 }
 
 
@@ -61,172 +61,198 @@ $estado = $pedidoarray['PEDEST'];
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Infoshop | Las mejoras ofertas en Informática!</title>
+  <link rel="stylesheet" type="text/css" href="../css/estilo-misc.css">
   <link rel="stylesheet" type="text/css" href="../css/estilo-rastrear.css">
   <link rel="stylesheet" type="text/css" href="js/slick/slick.css">
-<link rel="stylesheet" type="text/css" href="js/slick/slick-theme.css">
-<link rel="icon" type="image/png" href="img/Logo.png">
+  <link rel="stylesheet" type="text/css" href="js/slick/slick-theme.css">
+  <link rel="icon" type="image/png" href="img/Logo.png">
 </head>
+
 <body>
-<script src="js/jquery-3.7.0.min.js"></script>
-<script src="js/slick/slick.min.js"></script>
-<script src="js/slider.js"></script>
-<script src="js/dropdown.js"></script>
-<script src="js/dropdowntienda.js"></script>
-<script src="js/carrito.js"></script>
-<script src="js/redireccionar-producto.js"></script>
-<script src="js/carrito-checkout.js"></script>
+  <script src="js/jquery-3.7.0.min.js"></script>
+  <script src="js/slick/slick.min.js"></script>
+  <script src="js/slider.js"></script>
+  <script src="js/dropdown.js"></script>
+  <script src="js/dropdowntienda.js"></script>
+  <script src="js/carrito.js"></script>
+  <script src="js/redireccionar-producto.js"></script>
+  <script src="js/carrito-checkout.js"></script>
 
-<div id="overlay"></div>
+  <div id="overlay"></div>
 
-<div class="contenedor">
-  <nav class="navbar">
-    <div class="logo">
-      <a href="index"><img src="img/Logo.png"></a>
+  <div class="contenedor">
+    <div id="myDropdownTienda" class="category-sidebar">
+
+      <?php
+      $categorias = "SELECT * FROM categorias";
+      $categoriasql = mysqli_query($conexion, $categorias);
+
+      while ($arraycat = mysqli_fetch_row($categoriasql)) {
+
+        ?>
+        <div class="categoria-item">
+          <img src="img/<?php echo $arraycat[0] ?>.png" class="cross" onclick="cerrarMenuTienda()">
+          <a href="buscar?categorias=<?php echo $arraycat[0] ?>"><?php echo $arraycat[1] ?></a>
+        </div>
+        <?php
+      }
+
+      ?>
+
     </div>
-    <ul class="nav-links">
-      <li class="categorias"><button onclick="mostrarMenuTienda()" class="dropbtn" id="menuButtonShop">
-          <div class="button-content-tienda">
-          <img id="rayas" src="img/rayas.png">   
-          <h3>Todas las categorías</h3>  
-          </div>
-            <div id="myDropdownTienda" class="dropdown-content">
-              <?php
-            $categorias = "SELECT * FROM categorias";
-            $categoriasql= mysqli_query($conexion, $categorias);
 
-            while($arraycat = mysqli_fetch_row($categoriasql)){
-
-            ?>
-              <a href="buscar?categorias=<?php echo $arraycat[0] ?>"><?php echo $arraycat[1] ?></a>
-
-            <?php
-            }
-?>
-            </div>      
-    </li>
-      <li class="busqueda">
-        <!-- Formulario de búsqueda -->
-        <form method="GET" action="buscar" class="busqueda-formulario">
+    <nav class="navbar">
+      <div class="logo">
+        <a href="index"><img src="img/Logo.png"></a>
+      </div>
+      <ul class="nav-links">
+        <li class="categorias"><button onclick="mostrarMenuTienda()" class="dropbtn" id="menuButtonShop">
+            <div class="button-content-tienda">
+              <img id="rayas" src="img/rayas.png">
+              <h3>Todas las categorías</h3>
+            </div>
+        </li>
+        <li class="busqueda">
+          <!-- Formulario de búsqueda -->
+          <form method="GET" action="buscar" class="busqueda-formulario" onsubmit="aplicarFiltro()">
             <input type="text" name="query" class="busqueda-texto" placeholder="Buscar productos">
             <input type="submit" value="" class="busqueda-boton">
-        </form>
-      </li>
-      <li class="carrito">
-            <div class="cart-menu" onclick="toggleCartSidebar()">
-                <img src="img/carrito.png" alt="Carrito de compras">
-                <span>Mi carrito</span>
-            </div>
-            <div class="cart-sidebar" id="cart-sidebar" style="text-align: left;">
-                <p style="font-size:20px">Mi carrito</p>
-                <?php 
-                if(isset($_SESSION['usucod'])){
-
-                ?>  
-
-                <ul id="cart-items" class="cart-items"></ul>
-                <div class="cart-sidebar-footer" id="cart-sidebar-footer">
-                <p id="cart-total">TOTAL  0.00€</p>
-                <button id="carritoVaciarBtn" class="carrito-vaciar-btn" onclick="emptyCart()">Vacíar carrito</button>
-                <a  id="carritoCheckout" class="carrito-checkout" onclick="checkout()">Ver artículos del carrito</a>
-
-                <?php
-                }else {
-                  ?>
-
-                  <img id="carrito-error" src="img/carritoerror.png">
-                  <p id="login-carrito">Inicia sesión para utilizar el carrito.</p>
-
-                  <?php
-                }
-
-                ?>
-                </div>
-           
-      </li>
-      <?php
-            if(isset($_SESSION['usucod'])){
-              if($usuadm == 1){
-                ?>
-                <a href="panel-administrador?productos" id="administrador"><li class="panel-adm">
-                  <div class="panel-administrador">
-                    <img src="img/panel.png">
-                    <span>Panel de administrador</span>
-                  </div>
-                </li></a>
-                <?php
-              }
-            }
+          </form>
+        </li>
+        <?php
+        if (isset($_SESSION['usucod'])) {
+          if ($usuadm == 1) {
             ?>
-    </ul>
-    <ul class="sesiones">
-      <?php 
+            <a href="panel-administrador?productos" id="administrador">
+              <li class="panel-adm">
+                <div class="panel-administrador">
+                  <img src="img/panel.png">
+                  <span>Panel de administrador</span>
+                </div>
+              </li>
+            </a>
+            <?php
+          }
+        }
+        ?>
+      </ul>
+      <ul class="sesiones">
+        <?php
 
-        if(isset($_SESSION['usucod'])){
+        if (isset($_SESSION['usucod'])) {
           $usuloginsesion = $_SESSION['usunom'];
           ?>
           <div class="cuenta">
-          <button onclick="mostrarMenu()" class="dropbtn" id="menuButton">
-          <div class="button-content">
-          <div class="perfil-imagen">
-          <img class ="userlogo" src="<?php echo $usuimg ?>">
-          </div>
-          <h3><?php echo $usuloginsesion ?></h3>
-          <img id="flecha" src="img/flecha.png">
-          </div>
-            <div id="myDropdown" class="dropdown-content menu-contenedor">
-              <a href="perfil">Mi cuenta</a>
-              <a href="historial">Historial de pedidos</a>
-              <a href="#" onclick="cerrarSesion()">Cerrar sesión</a>
-            </div>
+            <button onclick="mostrarMenu()" class="dropbtn" id="menuButton">
+              <div class="button-content">
+                <div class="perfil-imagen">
+                  <img class="userlogo" src="<?php echo $usuimg ?>">
+                </div>
+                <h3><?php echo $usuloginsesion ?></h3>
+                <img id="flecha" src="img/flecha.png">
+              </div>
+              <div id="myDropdown" class="dropdown-content menu-contenedor">
+                <a href="perfil">Mi cuenta</a>
+                <a href="historial">Historial de pedidos</a>
+                <a href="#" onclick="cerrarSesion()">Cerrar sesión</a>
+              </div>
             </button>
-         </div>
+          </div>
 
-         <?php 
-        }else{
-          ?> 
-          <a href='login'><li>Iniciar sesión</li></a>
-                <a href='register'><li>Registrarse</li></a>
+
+          <?php
+        } else {
+          ?>
+          <a href='login'>
+            <li>Iniciar sesión</li>
+          </a>
+          <a href='register'>
+            <li>Registrarse</li>
+          </a>
           <?php
         }
-      
-      
-      ?>
-    </ul>
-  </nav>
-        <div id="main-container">
-        <div class="bloque-error">
-        <h1>Rastreando tu pedido</h1>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3221.234209976788!2d-5.3505230879797185!3d36.16085490336239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0cc08bd9eca285%3A0xb3a03205e3ba0bd5!2sOficina%20de%20Correos!5e0!3m2!1sen!2ses!4v1685362395724!5m2!1sen!2ses" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <div class="estado"><h3 style="color: white">Estado:</h3><p><?php echo $estado ?></div>
-        </div>
-        </div>
 
 
-  <!-- Resto del contenido de la página -->
-  <footer>
-  <div class="contenedor-footer">
-    <div class="logo-footer">
-      <img src="img/Logo.png" alt="Logo">
-      <h3>Infoshop</h3>
+        ?>
+        <div class="carrito">
+          <div class="cart-menu" onclick="toggleCartSidebar()">
+            <p id="productCount">0</p>
+            <img src="img/carrito.png" alt="Carrito de compras">
+            <span>Mi carrito</span>
+          </div>
+          <div class="cart-sidebar" id="cart-sidebar" style="text-align: left;">
+            <div class="cart-title">
+              <p style="font-size:20px">Mi carrito</p><img src="img/cross.png" alt="Cerrar" class="cart-close"
+                onclick="toggleCartSidebar()">
+            </div>
+            <?php
+            if (isset($_SESSION['usucod'])) {
+
+              ?>
+
+              <ul id="cart-items" class="cart-items"></ul>
+              <p id="empty-message">No hay artículos en tu carrito</p>
+              <div class="cart-sidebar-footer" id="cart-sidebar-footer">
+                <p id="cart-total">TOTAL 0.00€</p>
+                <button id="carritoVaciarBtn" class="carrito-vaciar-btn" onclick="emptyCart()">Vacíar carrito</button>
+                <a id="carritoCheckout" class="carrito-checkout" onclick="checkout()">Ver artículos del carrito</a>
+
+                <?php
+            } else {
+              ?>
+
+                <img id="carrito-error" src="img/carritoerror.png">
+                <p id="login-carrito">Inicia sesión para utilizar el carrito.</p>
+
+                <?php
+            }
+
+            ?>
+            </div>
+
+          </div>
+      </ul>
+    </nav>
+    <div id="main-container">
+      <div class="bloque-error">
+        <div class="fila"><img src="img/envio.gif"><h1>Rastreando tu pedido</h1></div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3221.234209976788!2d-5.3505230879797185!3d36.16085490336239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0cc08bd9eca285%3A0xb3a03205e3ba0bd5!2sOficina%20de%20Correos!5e0!3m2!1sen!2ses!4v1685362395724!5m2!1sen!2ses"
+          width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="estado">
+          <h3 style="color: black">Estado:</h3>
+          <p><?php echo $estado ?>
+        </div>
+      </div>
     </div>
-    <div class="redes-sociales">
-      <a href="#" class="icono-social"><img src ="img/fblogo.png" alt="Facebook"></i></a>
-      <a href="#" class="icono-social"><img src="img/twlogo.png" alt="Twitter"></a>
-      <a href="#" class="icono-social"><img src="img/iglogo.png" alt="Instagram"></a>
-      <a href="#" class="icono-social"><img src="img/tklogo.png" alt="Tik Tok"></a>
-    </div>
-  <div class="derechos">
-    <p> Infoshop &copy; 2025</p>
-    <p><a href="contacto" class="politica">Contacto</a></p>
-    <p><a href="privacidad" class="politica">Política de Privacidad</a></p>
+
+
+    <footer>
+      <div class="contenedor-footer">
+        <div class="logo-footer">
+          <img src="img/Logo.png" alt="Logo">
+          <h3>Infoshop</h3>
+        </div>
+        <div class="redes-sociales">
+          <a href="#" class="icono-social"><img src="img/fblogo.png" alt="Facebook"></i></a>
+          <a href="#" class="icono-social"><img src="img/twlogo.png" alt="Twitter"></a>
+          <a href="#" class="icono-social"><img src="img/iglogo.png" alt="Instagram"></a>
+          <a href="#" class="icono-social"><img src="img/tklogo.png" alt="Tik Tok"></a>
+        </div>
+        <div class="derechos">
+          <p> Infoshop &copy; 2025</p>
+          <p><a href="contacto" class="politica">Contacto</a></p>
+          <p><a href="privacidad" class="politica">Política de Privacidad</a></p>
+        </div>
+      </div>
+    </footer>
+
+
   </div>
-</div>
-</footer>
-
-
-</div>
 
 
 
@@ -237,4 +263,5 @@ $estado = $pedidoarray['PEDEST'];
 
 
 </body>
+
 </html>
